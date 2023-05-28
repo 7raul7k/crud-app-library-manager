aecbdb2bf6e32721f9697191c6931ca8f3a92c35
Balanescu Raul <balanescuraul@gmail.com>
2023-05-28 19:09:56 +0300

    add main

---
+++ data.js
@@ -0,0 +1,62 @@
+let books=[
+    {
+      title: "Double Jeopardy",
+      author: "Franz Benjafield",
+      genre: "Action|Crime|Drama|Thriller",
+      year: 2006
+    },
+    {
+      title: "Lakeboat",
+      author: "Gennifer Credland",
+      genre: "Comedy",
+      year: 1924
+    },
+    {
+      title: "King Solomon's Mines",
+      author: "Tani Shovlar",
+      genre: "Action|Adventure|Romance",
+      year: 1982
+    },
+    {
+      title: "Target",
+      author: "Geoff Jeavons",
+      genre: "Action|Drama|Thriller",
+      year: 1948
+    },
+    {
+      title: "Spin",
+      author: "Ware Vanshin",
+      genre: "Children|Drama",
+      year: 1958
+    },
+    {
+      title: "Reuben, Reuben",
+      author: "Klement Zanotti",
+      genre: "Comedy|Drama|Romance",
+      year: 1995
+    },
+    {
+      title: "Long Dark Hall, The",
+      author: "Lion Yanyushkin",
+      genre: "Crime|Drama",
+      year: 1975
+    },
+    {
+      title: "Snow Cake",
+      author: "Hatty Cuvley",
+      genre: "Drama",
+      year: 2022
+    },
+    {
+      title: "I Am My Films - A Portrait of Werner Herzog (Was ich bin, sind meine Filme)",
+      author: "Micheline Pencot",
+      genre: "Documentary",
+      year: 2017
+    },
+    {
+      title: "Detour ",
+      author: "Leah Berndsen",
+      genre: "Thriller",
+      year: 1930
+    }
+  ]
